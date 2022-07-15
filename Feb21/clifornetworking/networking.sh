272a85c3f0c7e27a857fced85cb16fd8bf5adb60
raju <raju.lvmh@gmail.com>
2022-07-15 20:40:04 +0530

    Commit message

---
+++ Feb21/clifornetworking/networking.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# create a gcp network
+gcloud compute networks create 'my-vpc-from-cli' --subnet-mode 'custom' 
+gcloud compute networks subnets create web-useast1 --network my-vpc-from-cli  --range 10.10.0.0/24 --region us-east1
+gcloud compute networks subnets create web-uswest1 --network my-vpc-from-cli  --range 172.16.0.0/24 --region us-west1
+gcloud compute networks subnets create web-uscentral1 --network my-vpc-from-cli  --range 192.168.1.0/24 --region us-central1
+
